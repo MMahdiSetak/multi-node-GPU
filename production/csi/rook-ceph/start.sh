70a28c234edd0d332bdca162d6fa32d61d69ab41
MMahdiSetak <m.mahdi.setak@gmail.com>
2025-11-18 16:40:15 +0330

    add ceph setup script

--- production/csi/rook-ceph/start.sh
+++ production/csi/rook-ceph/start.sh
@@ -1,1 +1,21 @@
 git clone --single-branch --branch v1.18.7 https://github.com/rook/rook.git
+
+kubectl create -f crds.yaml -f common.yaml -f csi-operator.yaml -f operator.yaml
+kubectl create -f cluster.yaml
+
+
+kubectl create -f deploy/examples/toolbox.yaml
+kubectl -n rook-ceph exec -it deploy/rook-ceph-tools -- bash
+
+ceph status
+ceph osd status
+ceph df
+rados df
+
+kubectl create -f deploy/examples/csi/rbd/storageclass.yaml
+
+kubectl apply -f deploy/examples/filesystem.yaml
+kubectl create -f deploy/examples/csi/cephfs/storageclass.yaml
+
+kubectl apply -f deploy/examples/dashboard-loadbalancer.yaml
+kubectl -n rook-ceph get secret rook-ceph-dashboard-password -o jsonpath="{['data']['password']}" | base64 --decode && echo
