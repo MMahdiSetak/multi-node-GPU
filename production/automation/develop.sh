f90904dc2827309108cce04105abc3b0700debcf
MMahdiSetak <m.mahdi.setak@gmail.com>
2025-12-26 17:06:45 +0330

    Update develop.sh to use gpu-cluster inventory and enhance nginx container setup

--- production/automation/develop.sh
+++ production/automation/develop.sh
@@ -19,14 +19,21 @@ ansible -i inventory/mycluster/inventory.ini all -m ping
 
 ansible-playbook -i inventory/mycluster/inventory.ini cluster.yml --check
 
-ansible-playbook -i inventory/mycluster/inventory.ini cluster.yml -b -v
-ansible-playbook -i inventory/mycluster/inventory.ini cluster.yml --tags network -b -v
-ansible-playbook -i inventory/mycluster/inventory.ini reset.yml --become --become-user=root
+ansible-playbook -i inventory/gpu-cluster/inventory.ini cluster.yml -b -v \
+    --skip-tags=apps,metrics_server,ingress_nginx,helm,bootstrap-os.swap,bootstrap-os.packages | tee deploy.log
+
+ansible-playbook -i inventory/gpu-cluster/inventory.ini cluster.yml --tags network -b -v
+ansible-playbook -i inventory/gpu-cluster/inventory.ini reset.yml -b -v | tee reset.log
 
 # Offline install
 cd contrib/offline
-./generate_list.sh -i ../../inventory/mycluster/inventory.ini
+./generate_list.sh -i ../../inventory/gpu-cluster/inventory.ini
 ./manage-offline-files.sh
+nerdctl run \
+    --restart=always -d -p 8080:80 \
+    --volume ./offline-files:/usr/share/nginx/html/download \
+    --volume ./nginx.conf:/etc/nginx/nginx.conf \
+    --name nginx nginx:alpine
 
 export IMAGES_FROM_FILE=temp/images.list
 ./manage-offline-container-images.sh create
@@ -35,9 +42,5 @@ export DESTINATION_REGISTRY=worker-g02:5000
 ./manage-offline-container-images.sh register
 
 
-sudo --preserve-env=http_proxy,https_proxy,no_proxy docker run \
-        --restart=always -d -p 8080:80 \
-        --volume ./offline-files:/usr/share/nginx/html/download \
-        --volume ./nginx.conf:/etc/nginx/nginx.conf \
-        --name nginx nginx:alpine
+
 
