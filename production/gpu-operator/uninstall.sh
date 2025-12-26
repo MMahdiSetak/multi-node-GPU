29c12f15d1e2cf229e3449c764d3a58e7e3aad02
MMahdiSetak <m.mahdi.setak@gmail.com>
2025-12-26 14:50:46 +0330

    comment out clusterrole and clusterrolebinding deletion commands in uninstall script

--- production/gpu-operator/uninstall.sh
+++ production/gpu-operator/uninstall.sh
@@ -1,10 +1,10 @@
 helm uninstall gpu-operator -n gpu-operator
 kubectl delete namespace gpu-operator
 
-kubectl get clusterrole | grep gpu-operator
-kubectl get clusterrolebinding | grep gpu-operator
-kubectl delete clusterrole gpu-operator-node-feature-discovery gpu-operator-node-feature-discovery-gc gpu-operator-node-feature-discovery gpu-operator-node-feature-discovery-gc
-kubectl delete clusterrolebinding gpu-operator gpu-operator-node-feature-discovery gpu-operator-node-feature-discovery-gc gpu-operator-node-feature-discovery gpu-operator-node-feature-discovery-gc
+# kubectl get clusterrole | grep gpu-operator
+# kubectl get clusterrolebinding | grep gpu-operator
+# kubectl delete clusterrole gpu-operator-node-feature-discovery gpu-operator-node-feature-discovery-gc gpu-operator-node-feature-discovery gpu-operator-node-feature-discovery-gc
+# kubectl delete clusterrolebinding gpu-operator gpu-operator-node-feature-discovery gpu-operator-node-feature-discovery-gc gpu-operator-node-feature-discovery gpu-operator-node-feature-discovery-gc
 
 kubectl get crd | grep -i nvidia
 kubectl delete crd clusterpolicies.nvidia.com nvidiadrivers.nvidia.com
