02589e019318a841306ca5f20286c2d48586d407
MMahdiSetak <m.mahdi.setak@gmail.com>
2026-02-27 22:30:24 +0330

    test kubecost

--- production/automation/install-all.sh
+++ production/automation/install-all.sh
@@ -19,67 +19,67 @@ KUBESPRAYDIR=kubespray
 source $VENVDIR/bin/activate
 cd $KUBESPRAYDIR
 
-extra_vars=$(cat <<EOF
-registry_host: "$REGISTRY"
-containerd_registries_mirrors:
-  - prefix: "$REGISTRY"
-    mirrors:
-      - host: "http://$REGISTRY"
-        capabilities: ["pull", "resolve"]
-        skip_verify: true
-files_repo: "$FILES_REPO"
-harbor: "https://$HARBOR_IP"
-EOF
-)
+# extra_vars=$(cat <<EOF
+# registry_host: "$REGISTRY"
+# containerd_registries_mirrors:
+#   - prefix: "$REGISTRY"
+#     mirrors:
+#       - host: "http://$REGISTRY"
+#         capabilities: ["pull", "resolve"]
+#         skip_verify: true
+# files_repo: "$FILES_REPO"
+# harbor: "https://$HARBOR_IP"
+# EOF
+# )
 
-ansible-playbook -i inventory/gpu-cluster/inventory.ini cluster.yml -b -v \
-    -e "$extra_vars" \
-    --skip-tags=metrics_server,ingress_nginx,helm,bootstrap-os.swap,bootstrap-os.packages | tee ../deploy.log
+# ansible-playbook -i inventory/gpu-cluster/inventory.ini cluster.yml -b -v \
+#     -e "$extra_vars" \
+#     --skip-tags=metrics_server,ingress_nginx,helm,bootstrap-os.swap,bootstrap-os.packages | tee ../deploy.log
 
-ansible master -i inventory/gpu-cluster/inventory.ini -b --become-user=root \
-  -m fetch \
-  -a "src=/etc/kubernetes/admin.conf dest=~/.kube/config flat=yes"
-MASTER_IP=$(ansible -i inventory/gpu-cluster/inventory.ini kube_control_plane \
-  -m debug -a "msg={{ ansible_host | default(inventory_hostname) }}" \
-  | grep msg | awk -F'"' '{print $4}')
-echo "Master IP is: $MASTER_IP"
-sed -i "s|127.0.0.1|${MASTER_IP}|g" ~/.kube/config
+# ansible master -i inventory/gpu-cluster/inventory.ini -b --become-user=root \
+#   -m fetch \
+#   -a "src=/etc/kubernetes/admin.conf dest=~/.kube/config flat=yes"
+# MASTER_IP=$(ansible -i inventory/gpu-cluster/inventory.ini kube_control_plane \
+#   -m debug -a "msg={{ ansible_host | default(inventory_hostname) }}" \
+#   | grep msg | awk -F'"' '{print $4}')
+# echo "Master IP is: $MASTER_IP"
+# sed -i "s|127.0.0.1|${MASTER_IP}|g" ~/.kube/config
 
-kubectl taint node master node-role.kubernetes.io/control-plane:NoSchedule-
+# kubectl taint node master node-role.kubernetes.io/control-plane:NoSchedule-
 
-ansible-playbook -i inventory/gpu-cluster/inventory.ini playbooks/tmp-registry.yml -b -v
+# ansible-playbook -i inventory/gpu-cluster/inventory.ini playbooks/tmp-registry.yml -b -v
 
-cd $ROOT_DIR/LB
-printf "\n▶ installing LB\n"
-bash ./apply-lbippool.sh
+# cd $ROOT_DIR/LB
+# printf "\n▶ installing LB\n"
+# bash ./apply-lbippool.sh
 
-cd $ROOT_DIR/csi/rook-ceph
-printf "\n▶ installing ceph\n"
-bash ./install.sh
+# cd $ROOT_DIR/csi/rook-ceph
+# printf "\n▶ installing ceph\n"
+# bash ./install.sh
 
-cd $ROOT_DIR/harbor
-printf "\n▶ installing harbor\n"
-bash ./install.sh
+# cd $ROOT_DIR/harbor
+# printf "\n▶ installing harbor\n"
+# bash ./install.sh
 
-cd $SCRIPT_DIR/kubespray
-ansible-playbook -i inventory/gpu-cluster/inventory.ini playbooks/main-registry.yml -b -v \
-    -e "$extra_vars" \
+# cd $SCRIPT_DIR/kubespray
+# ansible-playbook -i inventory/gpu-cluster/inventory.ini playbooks/main-registry.yml -b -v \
+#     -e "$extra_vars" \
 
-cd $ROOT_DIR/keycloak
-printf "\n▶ installing keycloak\n"
-bash ./install.sh
+# cd $ROOT_DIR/keycloak
+# printf "\n▶ installing keycloak\n"
+# bash ./install.sh
 
-cd $ROOT_DIR/monitoring
-printf "\n▶ installing monitoring\n"
-bash ./install.sh
+# cd $ROOT_DIR/monitoring
+# printf "\n▶ installing monitoring\n"
+# bash ./install.sh
 
-cd $ROOT_DIR/gpu-operator
-printf "\n▶ installing gpu-operator\n"
-bash ./install.sh
+# cd $ROOT_DIR/gpu-operator
+# printf "\n▶ installing gpu-operator\n"
+# bash ./install.sh
 
-cd $ROOT_DIR/kubeflow
-printf "\n▶ installing kubeflow\n"
-bash ./install.sh
+# cd $ROOT_DIR/kubeflow
+# printf "\n▶ installing kubeflow\n"
+# bash ./install.sh
 
 cd $ROOT_DIR/kubecost
 printf "\n▶ installing kubecost\n"
